187027c292f763e757ac767ccb26a7bd5f1c8898
malzaneght <malzaneght@gmail.com>
2018-02-13 18:39:08 +0300

    Script with readme

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
-# Password Strength Calculator
+Password Strength Calculator
 
-[TODO. There will be project description]
+Program can calculate strength of password. Scale of strenth is from 1 (the weakest) to 10 (the strongest). Requirements to password:
+1. Length
+2. Incoming of upper-case and lower-case letters, digits, special characters
 
-# Project Goals
+Use console: python password_strength.py
+
+Project Goals
 
 The code is written for educational purposes. Training course for web-developers - [DEVMAN.org](https://devman.org)
--- password_strength.py
+++ password_strength.py
@@ -1,8 +1,42 @@
+import re
+import getpass
+
+
+def get_length_strength(password):
+    good_length = 10
+    normal_length = 8
+    bad_length = 5
+    if len(password) > good_length:
+        return 3
+    elif len(password) > normal_length:
+        return 2
+    elif len(password) > bad_length:
+        return 1
+    else:
+        return 0
+
+
+def get_character_diversity(password):
+    counter = 0
+    array_patterns_to_check = ['[a-z]', '[A-Z]', '\d', '\W']
+    for pattern in array_patterns_to_check:
+        match = re.search(pattern, password)
+        if match:
+            counter += 1
+    return counter
 
 
 def get_password_strength(password):
-    pass
+    strength_length = get_length_strength(password)
+    strength_diversity = get_character_diversity(password)
+    total_strength = sum((strength_length, strength_diversity))
+    return total_strength
+
+
+def main():
+    password = getpass.getpass('Input password to check: ')
+    print('{}: {}'.format('Strength of your password', get_password_strength(password)))
 
 
 if __name__ == '__main__':
-    pass
+    main()
